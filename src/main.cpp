41e3f514a74d14aba060157a5891bd6a568eb7fb
Daniel Rahme <daniel.rahme.dr@gmail.com>
2020-03-17 12:49:07 +0100

    Added set and toggle functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define PIN PIN4
+
+template<typename T>
+void set_pin(T& port, int pin) {
+    port |= 1 << pin;
+}
+
+template<typename T>
+void toggle_pin(T& port, int pin) {
+    port ^= 1 << pin;
+}
 
 int main() {
-    auto toggle = [](auto pin){
-        DDRD |= 1 << pin;
-        PORTD ^= 1 << pin;
-    };
-
+    set_pin(DDRD, PIN);
 
     while(1){
-        toggle(PIN4);
-        _delay_ms(2000);
+        //toggle(PIN);
+        toggle_pin(PORTD, PIN);
+        _delay_ms(1000);
     }
 }
 
