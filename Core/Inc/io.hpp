12432e1179f02a512ce927153daad0eb2317254e
Daniel Rahme <daniel.rahme.dr@gmail.com>
2020-10-26 16:15:31 +0100

    Make io a class

--- Core/Inc/io.hpp
+++ Core/Inc/io.hpp
@@ -1,32 +1,35 @@
 #ifndef _IO_HPP
 #define _IO_HPP
 
-#define HIGH 1
-#define LOW  0
+class io {
 
-namespace io {
+public:
+    enum state {
+        high = 1,
+        low = 0
+    };
 
-enum Pin {
-    R_1 = 0,
-    R_2 = 1,
-    R_3 = 2,
-    R_4 = 3,
-    R_5 = 4,
-    R_6 = 5,
-    R_7 = 6,
-    R_8 = 7,
-    C_1 = 8,
-    C_2 = 9,
-    C_3 = 10,
-    C_4 = 11,
-    C_5 = 12,
-    C_6 = 13,
-    C_7 = 14,
-    C_8 = 15
+    enum pin {
+        R_1 = 0,
+        R_2 = 1,
+        R_3 = 2,
+        R_4 = 3,
+        R_5 = 4,
+        R_6 = 5,
+        R_7 = 6,
+        R_8 = 7,
+        C_1 = 8,
+        C_2 = 9,
+        C_3 = 10,
+        C_4 = 11,
+        C_5 = 12,
+        C_6 = 13,
+        C_7 = 14,
+        C_8 = 15
+    };
+
+    // Functions
+    static void write_pin(pin p, state value);
 };
-
-void write_pin(Pin pin, int value);
-
-} // End namespace
 
 #endif
--- Core/Src/io.cpp
+++ Core/Src/io.cpp
@@ -4,21 +4,20 @@
 
 //#include "stm32f303xc.h"
 
-namespace io {
-
 struct pin_t {
     GPIO_TypeDef* port;
     uint16_t pin_num;
 };
 
-const pin_t R1 = {ROW_1_GPIO_Port, ROW_1_Pin};
-const pin_t R2 = {ROW_2_GPIO_Port, ROW_2_Pin};
-const pin_t R3 = {ROW_3_GPIO_Port, ROW_3_Pin};
-const pin_t R4 = {ROW_4_GPIO_Port, ROW_4_Pin};
-const pin_t R5 = {ROW_5_GPIO_Port, ROW_5_Pin};
-const pin_t R6 = {ROW_6_GPIO_Port, ROW_6_Pin};
-const pin_t R7 = {ROW_7_GPIO_Port, ROW_7_Pin};
-const pin_t R8 = {ROW_8_GPIO_Port, ROW_8_Pin};
+// Constants PINS
+const pin_t R1 = { ROW_1_GPIO_Port, ROW_1_Pin };
+const pin_t R2 = { ROW_2_GPIO_Port, ROW_2_Pin };
+const pin_t R3 = { ROW_3_GPIO_Port, ROW_3_Pin };
+const pin_t R4 = { ROW_4_GPIO_Port, ROW_4_Pin };
+const pin_t R5 = { ROW_5_GPIO_Port, ROW_5_Pin };
+const pin_t R6 = { ROW_6_GPIO_Port, ROW_6_Pin };
+const pin_t R7 = { ROW_7_GPIO_Port, ROW_7_Pin };
+const pin_t R8 = { ROW_8_GPIO_Port, ROW_8_Pin };
 
 const pin_t C1 = { COL_1_GPIO_Port, COL_1_Pin };
 const pin_t C2 = { COL_2_GPIO_Port, COL_2_Pin };
@@ -29,12 +28,10 @@ const pin_t C6 = { COL_6_GPIO_Port, COL_6_Pin };
 const pin_t C7 = { COL_7_GPIO_Port, COL_7_Pin };
 const pin_t C8 = { COL_8_GPIO_Port, COL_8_Pin };
 
-const pin_t pins[16] = {R1, R2, R3, R4, R5, R6, R7, R8, C1, C2, C3, C4, C5, C6, C7, C8};
+const pin_t pins[16] = { R1, R2, R3, R4, R5, R6, R7, R8, C1, C2, C3, C4, C5, C6, C7, C8 };
 
-void write_pin(Pin pin, int value)
+void io::write_pin(io::pin p, io::state value)
 {
     GPIO_PinState state = (value ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(pins[pin].port, pins[pin].pin_num, state);
+    HAL_GPIO_WritePin(pins[p].port, pins[p].pin_num, state);
 }
-
-} // end namespace
