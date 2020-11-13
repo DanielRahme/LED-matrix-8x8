c116c3d4a22610242928c111c10ad759843b052a
Daniel Rahme <daniel.rahme.dr@gmail.com>
2020-11-13 19:56:22 +0100

    Using namespace instead of class

--- Core/Inc/io.hpp
+++ Core/Inc/io.hpp
@@ -1,14 +1,12 @@
 #ifndef _IO_HPP
 #define _IO_HPP
 
-class io {
- private:
+namespace io {
   static constexpr int max_column = 8;
   static constexpr int max_row = 8;
 
- public:
-  static void write_row(int row, unsigned char value);
-  static void pins_default();
-};
+  void write_row(int row, unsigned char value);
+  void pins_default();
+}
 
 #endif
