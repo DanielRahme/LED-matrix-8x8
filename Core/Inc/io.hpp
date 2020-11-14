c84a4db5fd5d2dfe6df9938d3e981005f802f7a1
Daniel Rahme <daniel.rahme.dr@gmail.com>
2020-11-14 18:13:22 +0100

    Made namespace in io.cpp and struct for pins

--- Core/Inc/io.hpp
+++ Core/Inc/io.hpp
@@ -1,9 +1,26 @@
 #ifndef _IO_HPP
 #define _IO_HPP
 
+#include "io.hpp"
+#include "main.h"
 namespace io {
+
   static constexpr int max_column = 8;
   static constexpr int max_row = 8;
+
+  namespace pins
+  {
+    template <typename PortType = decltype(ROW_1_GPIO_Port)>
+    struct pin_t {
+      PortType port;                        // GPIO_TypeDef* port;
+      int pin_num;
+
+      void set();
+      void reset();
+      //void toggle();
+    };
+
+  }
 
   void write_row(int row, unsigned char value);
   void pins_default();
--- Core/Src/io.cpp
+++ Core/Src/io.cpp
@@ -5,17 +5,27 @@
 
 //#include "stm32f303xc.h"
 
- template <typename Port>
- struct pin_t {
-   // GPIO_TypeDef* port;
-   Port port;
-   int pin_num;
- };
+// using pin = io:pin::pin_t<decltype(ROW_1_GPIO_Port)>;  // Define pin. Use the
+// port type here
 
-using pin = pin_t<decltype(ROW_1_GPIO_Port)>;  // Define pin. Use the port type here
+namespace io 
+{
 
+namespace pins {
 
-// Constants pins
+// using pin = io::pins::pin_t<decltype(ROW_1_GPIO_Port)>;
+
+template<typename PortType>
+void pin_t<PortType>::set() {
+  HAL_GPIO_WritePin(pin_t::port, pin_t::pin_num, GPIO_PIN_SET);
+}
+
+template<typename PortType>
+void pin_t<PortType>::reset() {
+  HAL_GPIO_WritePin(pin_t::port, pin_t::pin_num, GPIO_PIN_RESET);
+}
+
+using pin = io::pins::pin_t<>;  // io::pins::pin_t<decltype(ROW_1_GPIO_Port)>;
 constexpr pin R1 = {ROW_1_GPIO_Port, ROW_1_Pin};
 constexpr pin R2 = {ROW_2_GPIO_Port, ROW_2_Pin};
 constexpr pin R3 = {ROW_3_GPIO_Port, ROW_3_Pin};
@@ -36,44 +46,51 @@ constexpr pin C8 = {COL_8_GPIO_Port, COL_8_Pin};
 constexpr etl::array<pin, 8> pins_rows = {R1, R2, R3, R4, R5, R6, R7, R8};
 constexpr etl::array<pin, 8> pins_colums = {C1, C2, C3, C4, C5, C6, C7, C8};
 
-void set_pin(const pin p) {
+
+}  // End namespace pinso
+
+void set_pin(const io::pins::pin p) {
   HAL_GPIO_WritePin(p.port, p.pin_num, GPIO_PIN_SET);
 }
 
-void reset_pin(const pin p) {
+void reset_pin(const io::pins::pin p) {
   HAL_GPIO_WritePin(p.port, p.pin_num, GPIO_PIN_RESET);
 }
 
-void write_pin(const pin p, int value) {
-  HAL_GPIO_WritePin(p.port, p.pin_num, static_cast<decltype(GPIO_PIN_RESET)>(value));
+void write_pin(const io::pins::pin p, int value) {
+  HAL_GPIO_WritePin(p.port, p.pin_num,
+                    static_cast<decltype(GPIO_PIN_RESET)>(value));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Public funtions
 ////////////////////////////////////////////////////////////////////////////////
-void io::write_row(int row, unsigned char value) {
-
-  if (row < 0 || row > io::max_row) return;  // Boundry check, do with definition
+void write_row(int row, unsigned char value) {
+  if (row < 0 || row > io::max_row)
+    return;  // Boundry check, do with definition
 
   // Write to colums starting from leftmost one (MSB)
-  for (auto col : pins_colums) {
+  for (auto col : io::pins::pins_colums) {
     write_pin(col, value & 0b1000'0000);
     value <<= 1;
   }
 
   // Enable row
-  reset_pin(pins_rows.at(row));
+  reset_pin(io::pins::pins_rows.at(row));
 }
 
 // Turn off all LEDs
-void io::pins_default() {
+void pins_default() {
   // Active low led pin, disable with high
-  for (auto r : pins_rows) {
-    set_pin(r);
+  for (auto r : io::pins::pins_rows) {
+    // set_pin(r);
+    r.set();
   }
 
-  for (auto c : pins_colums) {
-    reset_pin(c);
+  for (auto c : io::pins::pins_colums) {
+    // reset_pin(c);
+    c.reset();
   }
 }
 
+}  // End namespace io
